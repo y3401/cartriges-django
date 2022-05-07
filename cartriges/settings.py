bd4e0c055edd48c5dc0b933ef8dae6cc65e25610
Yury <y3401@yandex.ru>
2022-05-07 19:31:27 +0300

    Update settings.py

--- cartriges/settings.py
+++ cartriges/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pcarts',
+        'NAME': 'carts',
         # docker
         'USER': 'root',
         'PASSWORD': 'paro1_mysq1',
